92466729f7a82bccb01e53310bb3728b34ed3a5c
Mary Melnikova <marymelnikova1988@gmail.com>
2024-02-24 16:16:03 +0300

    blog+index

--- mails/urls.py
+++ mails/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
 from mails.apps import MailsConfig
-from mails.views import base
+from mails.views import IndexView, contacts
 
 app_name = MailsConfig.name
 
 urlpatterns = [
-    path('', base),
+    path('', IndexView.as_view(), name='index'),
+    path('contacts/', contacts, name='contacts'),
+
 ]
--- mails/views.py
+++ mails/views.py
@@ -1,6 +1,30 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+from blog.models import Blog
 
-def base(request):
-    return render(request, 'mails/base.html')
+
+class IndexView(TemplateView):
+    template_name = 'mails/index.html'
+    extra_context = {
+        'title': 'Главная',
+    }
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data['object_list'] = Blog.objects.all()[:3]
+        return context_data
+
+
+def contacts(request):
+    context = {
+        'title': "Контакты",
+    }
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print(f"{name} ({phone}, {email}): {message}")
+
+    return render(request, 'mails/contacts.html', context)
